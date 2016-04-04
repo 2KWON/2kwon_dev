e1441fe8801c739960fbe3b3ef7c69bfd6fed6b1
Seokhwan <am400000@gmail.com>
2016-04-04 16:31:10 +0900

    pubList 추가

---
+++ public/main/pubList.js
@@ -0,0 +1,53 @@
+/**
+ * Created by Seokhwan on 2016. 3. 30..
+ */
+"use strict"
+//목록 불러오기
+//위도 경도 임시로 지정함.
+//역의 코드로 리스트 load함
+var station_code = 'sinlim';
+var phone_latitude    = "10";      //위
+var phone_longtitude  = "20";    //경도
+
+alert('hello');
+function getList(){
+    $.ajax({
+        type:"POST",
+        url:'/api/main/getPublist',
+        dataType:'json',
+        data:{
+            "latitude"   : phone_latitude,
+            "longtitude" : phone_longtitude,
+            "stationCode" : station_code
+        },
+        success:function(list){
+            var length = list.length;
+            console.log(list);
+            $('#locationName').text(list[0].station_code);
+            $('#subTitle').html('&nbsp;'+length+' of '+length+' Pubs &nbsp;|&nbsp;&nbsp;'+length/length*100+'% 영업중');
+            $('#titleImg').attr('src','/static/images/'+list[0].station_code+'.jpg');
+            makeList(list);
+        },
+        error:function(e){
+            alert(e.responseText);
+        }
+    });
+}
+
+function makeList(list){
+    $('#publist').empty();
+    var length = list.length;
+    for(var i = 0; i<length; i++){
+    var a ='<li class="list-item" data-ix="list-item">'+
+        '<a class="w-clearfix w-inline-block" href="#">'+
+            '<div class="icon-list">'+
+            '<div><img src="'+list[i].pub_img+'"></div>'+
+            '</div>'+
+            '<div class="title-list">'+list[i].pub_name+'</div>'+
+        '</a>'+
+        '</li>';
+        $('#publist').append(a);
+    }
+
+}
+getList();
