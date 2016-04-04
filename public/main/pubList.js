9a21504b8f0b25d77166fe101d90ae99c8b66d9b
Seokhwan <am400000@gmail.com>
2016-04-04 16:44:40 +0900

    pubList 추추가수정

--- public/main/pubList.js
+++ public/main/pubList.js
@@ -9,7 +9,7 @@ var station_code = 'sinlim';
 var phone_latitude    = "10";      //위
 var phone_longtitude  = "20";    //경도
 
-alert('hello');
+//alert('hello');
 function getList(){
     $.ajax({
         type:"POST",
@@ -26,21 +26,7 @@ function getList(){
             $('#locationName').text(list[0].station_code);
             $('#subTitle').html('&nbsp;'+length+' of '+length+' Pubs &nbsp;|&nbsp;&nbsp;'+length/length*100+'% 영업중');
             $('#titleImg').attr('src','/static/images/'+list[0].station_code+'.jpg');
-            //makeList(list);
-
-            $('#publist').empty();
-            var length = list.length;
-            for(var i = 0; i<length; i++){
-                var a ='<li class="list-item" data-ix="list-item">'+
-                    '<a class="w-clearfix w-inline-block" href="#">'+
-                    '<div class="icon-list">'+
-                    '<div><img src="'+list[i].pub_img+'"></div>'+
-                    '</div>'+
-                    '<div class="title-list">'+list[i].pub_name+'</div>'+
-                    '</a>'+
-                    '</li>';
-                $('#publist').append(a);
-            }
+            makeList(list);
         },
         error:function(e){
             alert(e.responseText);
@@ -49,7 +35,20 @@ function getList(){
 }
 
 function makeList(list){
-
+    //alert(makeList);
+    $('#publist').empty();
+    var length = list.length;
+    for(var i = 0; i<length; i++){
+    var a ='<li class="list-item" data-ix="list-item">'+
+        '<a class="w-clearfix w-inline-block" href="#">'+
+            '<div class="icon-list">'+
+            '<div><img src="'+list[i].pub_img+'"></div>'+
+            '</div>'+
+            '<div class="title-list">'+list[i].pub_name+'</div>'+
+        '</a>'+
+        '</li>';
+        $('#publist').append(a);
+    }
 
 }
 getList();
